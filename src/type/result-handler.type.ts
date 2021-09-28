a62a9673943954eb169592bd4c0a559f033297cf
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-09-28 19:17:55 +0200

    refactor(ResultHandler): extend generic type variable `Payload` by the object.

--- src/type/result-handler.type.ts
+++ src/type/result-handler.type.ts
@@ -1,7 +1,7 @@
 /**
  * Internal function to handle the arguments of the `ResultCallback` function before its result return.
  */
-export type ResultHandler<Value = any, Payload = object> = (
+export type ResultHandler<Value = any, Payload extends object = object> = (
   result: boolean,
   value: Value,
   payload?: Payload
